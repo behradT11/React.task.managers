87e192bbef890ebc514e13aa45ce0cbb110e51b7
behradT11 <141186716+behradT11@users.noreply.github.com>
2025-08-21 14:42:42 +0330

    Create Tailwind.config.js

---
+++ Tailwind.config.js
@@ -0,0 +1,32 @@
+** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'toast-in': 'toast-in 0.5s ease-out forwards',
+        'gradient-bg': 'gradient-bg 15s ease infinite',
+        'fade-in': 'fade-in 0.5s ease-out forwards',
+      },
+      keyframes: {
+        'toast-in': {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'gradient-bg': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+    },
+  },
+  plugins: [],
+}
