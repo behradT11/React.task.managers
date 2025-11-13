74bd39bc8dc9e4b40447cda84e3c31491788bb97
behradT11 <141186716+behradT11@users.noreply.github.com>
2025-11-13 08:03:48 +0330

    Delete Vite.config.ts

--- Vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // در اینجا نام ریپازیتوری خود را وارد کنید
-  base: '/React.task.managers/', 
-  plugins: [react()],
-})
